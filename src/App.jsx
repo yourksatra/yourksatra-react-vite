824024af2bf0c4cf8d5d4d272fd71a31a8814374
yourksatra <sb130074@gmail.com>
2025-09-16 22:00:17 +0700

    Release App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,6 @@ export default function App() {
                 setActivePage={setActivePage}
               />
               <ContactSection />
-              <ExperienceSection />
             </>
           )}
           {activePage === "ExperienceSection" && (
