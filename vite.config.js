0c906c9190970582ba9ee9e9cf16bd6d2df7c275
yourksatra <sb130074@gmail.com>
2025-10-13 18:55:51 +0700

    Vite Config Update

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base: process.env.VITE_BASE_PATH || "/yourksatra-react-vite",
-  server: {
-    host: true
-  }
+  base: process.env.VITE_BASE_PATH || "/yourksatra-react-vite",
 })
